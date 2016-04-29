ce422bf976543983b1109cde2a246e69f366b814
Dawit Nida <dchonch@gmail.com>
2016-04-29 17:02:42 +0300

    file sturcture created

---
+++ app/Palindrome.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Created by DawitNida.
+ * User: Dawit
+ * Date: 4/29/2016
+ * Time: 16:50
+ */
+class Palindrome
+{
+    public function prepare()
+    {
+    }
+
+    public function fetchUserInput()
+    {
+    }
+
+    public static function execInput()
+    {
+
+    }
+}
---
+++ tests/AssertUserInputTest.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Dawit
+ * Date: 4/29/2016
+ * Time: 16:46
+ */
+class AssertUserInputTest extends PHPUnit_Framework_TestCase
+{
+
+}
